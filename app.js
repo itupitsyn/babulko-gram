25aa160e00950ddf7dbd6d8c16bf473fe43465eb
Ilya <toupitsyn@gmail.com>
2022-02-23 19:16:39 +0300

    init

--- app.js
+++ app.js
@@ -2,19 +2,34 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const hbs = require('hbs');
-const indexRouter = require('./routers/index');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
 require('dotenv').config();
 
+const indexRouter = require('./routers/index');
+
 const app = express();
 const PORT = process.env.PORT ?? 3000;
+hbs.registerPartials(path.join(process.env.PWD, 'views', 'partials'));
 
 app.set('view engine', 'hbs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(express.static(path.join(process.PWD, 'public')));
-hbs.registerPartials(path.join(process.PWD, 'views', 'partials'));
+app.use(express.static(path.join(process.env.PWD, 'public')));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false, maxAge: 60 * 60 * 1000, httpOnly: true },
+    name: 'userCookie',
+    store: new FileStore(),
+  }),
+);
 
 app.use('/', indexRouter);
 
