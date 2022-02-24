37a00cc9081f3c629ad076c4cdef9542a05147f8
Viktor-Vrazumitel <Viktor_bogatyrev@mail.ru>
2022-02-24 19:20:48 +0300

    pull na pageUser

--- public/js/user.js
+++ public/js/user.js
@@ -2,14 +2,14 @@ const { postForm } = document.forms;
 const allEntries = document.querySelector('#allEntries');
 
 function createCard(data) {
-    const {entries} = data;
+    const { entries } = data;
 
     return `<div class="card" style="width: 18rem;">
     <img src="${entries.image}" class="card-img-top" alt="image">
     <div class="card-body">
       <h5 class="card-title">${entries.userId}</h5>
       <h2 class="card-title">${entries.text}</h2>`
-    
+
 }
 
 postForm.addEventListener('submit', async (e) => {
@@ -19,11 +19,11 @@ postForm.addEventListener('submit', async (e) => {
         method: 'POST',
         body: formData,
     });
-    
+
     if (response.ok) {
         const data = await entries.json();
         allEntries.insertAdjacentHTML('afterbegin', createCard(data));
-    }else {
+    } else {
         alert('Что то пошло не так, перезагрузите страницу!');
     }
 
