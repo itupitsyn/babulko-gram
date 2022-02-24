04cc26597b15969a9cd73f453476196164b16f41
Ilya <toupitsyn@gmail.com>
2022-02-24 20:35:05 +0300

    handles protection, password encryption

--- middlewares/allMiddleWares.js
+++ middlewares/allMiddleWares.js
@@ -1,5 +1,6 @@
-const addToLocals = (req,res,next) => {
-  res.locals.userName= req.session?.userName; //если req.session.userId сушествует, тогда запиши его в rec.locals
+const addToLocals = (req, res, next) => {
+  res.locals.userName = req.session?.userName;
+  res.locals.userId = req.session?.userId;
   next();
 };
 
@@ -7,18 +8,16 @@ const checkUser = (req, res, next) => {
   if (req.session.userName) {
     next(); // если юзер есть в сессии, пожалуйста, проходи дальше
   } else {
-    res.redirect('/entries/register') // если юзера нет, то надо залогиниться
+    res.redirect('/home/register'); // если юзера нет, то надо залогиниться
   }
-}
+};
 
 const deepCheckUser = (req, res, next) => {
-  // console.log('deepCheckUser');
-  // console.log(req.params.id, req.session.userId);
   if (Number(req.session.userId) === Number(req.params.id)) {
     next(); // если юзер есть в сессии, пожалуйста, проходи дальше
   } else {
-    res.redirect('/entries/register') // если юзера нет, то надо залогиниться
+    res.redirect('/home/register'); // если юзера нет, то надо залогиниться
   }
-}
+};
 
-module.exports = { addToLocals, checkUser, deepCheckUser};
+module.exports = { addToLocals, checkUser, deepCheckUser };
