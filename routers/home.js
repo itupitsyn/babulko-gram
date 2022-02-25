0b9f907c5bfb1e25352d1eb6831c7666b4c105cd
Mariana <m160294@mail.ru>
2022-02-25 20:14:36 +0300

    some css added and statuses for registration

--- routers/home.js
+++ routers/home.js
@@ -1,18 +1,33 @@
 const router = require('express').Router();
-const { User } = require('../db/models/');
+const { User, Status } = require('../db/models');
 
 const { checkUser, deepCheckUser } = require('../middlewares/allMiddleWares');
 
-router.get('/register', (req, res) => {
-  res.render('home/register');
-})
+router.get('/register', async (req, res) => {
+  const status = await Status.findAll();
+  res.render('home/register', { status });
+});
 
 router.get('/', (req, res) => {
   res.render('index');
-})
+});
 
 router.post('/register', async (req, res) => {
-  const user = await User.create(req.body);
+  const { login, email, password, name, statuses } = req.body;
+  console.log(req.body);
+  if (statuses === '' && name === '') {
+    res.send ('Поле "статус" не может быть пустым. Пожалуйста, введите статус');
+  };
+  let user;
+  if (statuses === '') {
+    const status = await Status.create({name});
+    user = await User.create( {login, email, password, statusId: status.id})
+  } else { 
+    let st = await Status.findOne({where: {name: statuses}})
+    console.log(st)
+    console.log(st.id)
+    user = await User.create( {login, email, password, statusId: st.id })
+  }
   req.session.userName = user.login;
   req.session.userEmail = user.email;
   req.session.userId = user.id;
@@ -20,7 +35,6 @@ router.post('/register', async (req, res) => {
 })
 
 router.post('/', async(req,res) => {
-  console.log('tut')
   const { email, password } = req.body;
   const user = await User.findOne( {where: {email: email}})
   if (user) {
@@ -31,16 +45,26 @@ router.post('/', async(req,res) => {
       res.redirect('/user') // дописать ручку
     } else {
       res.send ('Wrong password. Please try again');
-    }      
     }
-    res.redirect(`/home/register`)
+  } else {
+    res.redirect(`/home/register`);
+  }
 
 })
 
-router.get('/profile/:id', checkUser, deepCheckUser, async (req,res) => {
-  const user = await User.findByPk(req.params.id);
-  res.render('userPage', {user}); // переходим на userPage.hbs
+router.get('/', (req,res) => {
+  const { statusFromUser } = req.body;
+  const status = Status.findOne({ where: {status} })
+})
 
+router.get('/user', checkUser, deepCheckUser, async (req,res) => {
+  const user = await User.findByPk(req.params.id);
+  res.render('/user', {user}); // переходим на user.hbs
+
+})
+
+router.get('/user', (req, res) => {
+  res.redirect('/home/user');
 })
 
 
