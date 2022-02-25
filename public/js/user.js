2cca640748d8a4fad2fd3a5465581d6b02dd9757
Ilya <toupitsyn@gmail.com>
2022-02-25 10:28:56 +0300

    merging

--- public/js/user.js
+++ public/js/user.js
@@ -1,32 +1,16 @@
-// const { postForm } = document.forms;
-// const allEntries = document.querySelector('#allEntries');
+const { postForm } = document.forms;
+const allEntries = document.querySelector('#allEntries');
 
- function createCard(data) {
-   const { entries } = data;
+function createCard(data) {
+  const { entries } = data;
 
-   return `<div class="card" style="width: 18rem;">
+  return `<div class="card" style="width: 18rem;">
      <img src="${entries.image}" class="card-img-top" alt="image">
      <div class="card-body">
      <h5 class="card-title">${entries.userId}</h5>
        <h2 class="card-title">${entries.text}</h2>`;
 }
 
-<<<<<<< HEAD
- postForm.addEventListener('submit', async (e) => {
-   e.preventDefault();
-   const formData = new FormData(postForm);
-   const response = await fetch('/api/entries', {
-     method: 'POST',
-     body: formData,
-   });
-   if (response.ok) {
-     const data = await response.json();
-     allEntries.insertAdjacentHTML('afterbegin', createCard(data));
-   } else {
-     alert('Что то пошло не так, перезагрузите страницу!');
-   }
- });
-=======
 postForm.addEventListener('submit', async (e) => {
   e.preventDefault();
   const formData = new FormData(postForm);
@@ -42,4 +26,3 @@ postForm.addEventListener('submit', async (e) => {
     alert('Что то пошло не так, перезагрузите страницу!');
   }
 });
->>>>>>> main
--- routers/home.js
+++ routers/home.js
@@ -15,28 +15,28 @@ router.get('/', (req, res) => {
 
 router.post('/register', async (req, res) => {
   const { login, email, name, statuses } = req.body;
-  password = crypto
+  let password = crypto
     .createHash('sha256')
     .update(req.body.password)
     .digest('hex');
   if (statuses === '' && name === '') {
-    res.send ('Поле "статус" не может быть пустым. Пожалуйста, введите статус');
-  };
+    res.send('Поле "статус" не может быть пустым. Пожалуйста, введите статус');
+  }
   let user;
   if (statuses === '') {
-    const status = await Status.create({name});
-    user = await User.create( {login, email, password, statusId: status.id})
-  } else { 
-    let st = await Status.findOne({where: {name: statuses}})
-    user = await User.create( {login, email, password, statusId: st.id })
+    const status = await Status.create({ name });
+    user = await User.create({ login, email, password, statusId: status.id });
+  } else {
+    const st = await Status.findOne({ where: { name: statuses } });
+    user = await User.create({ login, email, password, statusId: st.id });
   }
   req.session.userName = user.login;
   req.session.userEmail = user.email;
   req.session.userId = user.id;
-  res.redirect('/user') // дописать ручку
-})
+  res.redirect('/user'); // дописать ручку
+});
 
-router.post('/', async(req,res) => {
+router.post('/', async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ where: { email } });
   if (user) {
@@ -49,29 +49,26 @@ router.post('/', async(req,res) => {
       req.session.userId = user.id;
       res.redirect('/user'); // дописать ручку
     } else {
-      res.send ('Неправильный пароль. Пожалуйста, попррбуйте еще раз!');
+      res.send('Неправильный пароль. Пожалуйста, попррбуйте еще раз!');
     }
   } else {
-    res.redirect(`/home/register`);
+    res.redirect('/home/register');
   }
+});
 
-})
-
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
   const { statusFromUser } = req.body;
-  const status = Status.findOne({ where: {status} })
-})
+  const status = Status.findOne({ where: { status } });
+});
 
-router.get('/user', checkUser, deepCheckUser, async (req,res) => {
+router.get('/user', checkUser, deepCheckUser, async (req, res) => {
   const user = await User.findByPk(req.params.id);
-  res.render('/user', {user}); // переходим на user.hbs
-
-})
+  res.render('/user', { user }); // переходим на user.hbs
+});
 
 // router.get('/user', (req, res) => {
 //   res.redirect('/home/user');
 // })
-
 
 router.get('/logout', (req, res) => {
   req.session.destroy();
